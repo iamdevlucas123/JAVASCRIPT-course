453c3988b057167a3f22cadaae39414fca4d1148
Lucas <lucas@gmail.com>
2025-09-24 17:09:36 -0500

    welcome name

--- 00-EXERCICIOS+/06-contaDeBanco/script.js
+++ 00-EXERCICIOS+/06-contaDeBanco/script.js
@@ -1,1 +1,11 @@
-const nome = 
+const nome = prompt("Digite seu nome: ")
+const welcome = document.getElementById('welcome')
+welcome.append(nome)
+
+function depositar(){
+    let saldo = document.getElementById('saldo').value
+    let depositar = document.getElementById('depositar').value
+
+    depositar += saldo
+}
+
