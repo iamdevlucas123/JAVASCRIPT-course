bfa7ef94b83ebf56540cb0d531695c22c639b6b6
Lucas <lucas@gmail.com>
2025-09-27 16:06:23 -0500

    update with comments

--- 12-JSON + localStorage/miniProject/script.js
+++ 12-JSON + localStorage/miniProject/script.js
@@ -1,32 +1,77 @@
+
 document.addEventListener("DOMContentLoaded", carregarTarefas)
 
-//"DOMContentLoaded" – This is the event being listened for. It fires once the HTML document has been completely loaded and parsed, but before images, stylesheets, and other resources finish loading.
+function adicionarTarefas() {
 
-function adicionarTarefas(){
-    const input = document.getElementById('taskInput')
-    const tarefa = input.value.trim();
+    //Pegar o valor digitado:
+    const input = document.getElementById("taskInput");
+    const task = input.value.trim();
 
-    if (tarefa === ""){
-        alert('Digite uma tarefa!')
-        return;
+    //condição para ver. se input está vazio
+    if (task === ""){
+        alert('Please type something!')
     }
 
-    // Passo 1: Buscar tarfeas existentes no localStorage
-    //Pega o valor armazenado no navegador com a chave "tasks"
-    //JSON.parse() : Converte a string de volta para um objeto ou array JavaScript.
-    //O || [] garante que tarefas será sempre um array, mesmo que esteja vazio.
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    //Buscar tarefas já existentes dentro do localStorage ou um array vazio
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
-    //Passo 2: Adicionar nova tarefa no array
-    tasks.push(tarefa)
+    //Adicionar uma nova tarefa dentro do array
+    tarefas.push(task)
 
+    //Salvar novamente no localStorage e transformar em um texto JSON
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
 
-    //Passo 3: Salvar novamente no localStorage
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-
-    //passo 4: Atualizar lista na tela:
+    //Atualizar a lista na tela
     exibirTarefas()
 
     // Limpar input
     input.value = "";
+
+}
+
+function exibirTarefas(){
+    //Pegar a lista:
+    const lista = document.getElementById('taskList')
+    lista.innerHTML = "";
+
+    // Recupera as tarefas do localStorage
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+    //Criar cada item na tela:
+    tarefas.forEach((tarefa, index) =>{
+
+        //Criar uma li na lista
+        const li = document.createElement("li");
+        li.textContent = tarefa; //??
+
+        //botão de remover
+        const btn = document.createElement("button");
+        btn.textContent = "X";
+
+        //Função que remove a tarefa
+        btn.onclick = () => removerTarefa(index);
+
+        //Cria um botão e tarefa
+        li.appendChild(btn);
+        lista.appendChild(li);
+    })
+}
+
+function removerTarefa(index){
+
+    //Recupera tarefas no localStorage
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+    //Remove pelo Índice
+    tarefas.splice(index, 1);
+
+    //Salva lista atualizada
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+    //Atualiza a lista na tela
+    exibirTarefas()
+}
+
+function carregarTarefas() {
+    exibirTarefas()
 }
